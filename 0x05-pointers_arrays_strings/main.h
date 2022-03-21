6debf668d616f46c8ca6e5c7c5959c89bb0b8992
MadivaLenny <madivalenny@gmail.com>
2022-03-22 00:21:08 +0300

    Header file

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,38 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* _putchar */
+int _putchar(int n);
+
+/* 0.98 BAttery st */
+void reset_to_98(int *n);
+
+/* Dont swap horses in a crossing stream */
+void swap_int(int *a, int *b);
+
+/* 2 report */
+int _strlen(char *s);
+
+/* 3. lack */
+void _puts(char *str);
+
+/* 4. reverse gear */
+void print_rev(char *s);
+
+/* 5. Engineer */
+void rev_string(char *s);
+
+/* 6. lies */
+void puts2(char *str);
+
+/* 7. Winning */
+void puts_half(char *str);
+
+/* 8. Arrays not pointers */
+void print_array(int *a, int n);
+
+/* 9. strcpy */
+char *_strcpy(char *dest, char *src);
+
+int _atoi(char *s);
+#endif
