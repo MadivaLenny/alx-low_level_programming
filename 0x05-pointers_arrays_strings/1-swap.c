876475d1bc45b1ab1aa5613d40041da3379957f2
MadivaLenny <madivalenny@gmail.com>
2022-03-22 00:22:39 +0300

    Function that takes a pointetr to an int as parameter and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *swap_int - swaps the value of two intergers
+ *@a: first pointer
+ *@b: second pointer
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+
+	*a = *b;
+	*b = c;
+}
